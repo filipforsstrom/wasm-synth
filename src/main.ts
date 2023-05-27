394c95a6ff8d5cf23533d05d47ec58c0bb1593f8
filipforsstrom <filipforsstrom@gmail.com>
2023-05-27 21:13:08 +0200

    working cpal beep example

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import "./style.css";
-import init, { greet } from "../wasm/pkg";
+import init, { beep, Handle } from "../wasm/pkg";
 
-init().then(() => {
-  greet();
-});
+init()
+  .then(() => {
+    let handle: Handle | null = null;
+    const playButton = document.getElementById("play")!;
+    playButton.addEventListener("click", () => {
+      handle = beep();
+    });
+    const stopButton = document.getElementById("stop")!;
+    stopButton.addEventListener("click", () => {
+      if (handle !== null) {
+        handle.free();
+        handle = null;
+      }
+    });
+  })
+  .catch(console.error);
